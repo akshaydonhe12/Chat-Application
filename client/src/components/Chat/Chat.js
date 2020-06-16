172738e78415c58acbc1f9af3932edc9815be8fa
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-06-16 12:04:25 +0530

    fix-bug-queryString

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,7 +3,16 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 
 
-const Chat = () => {
+const Chat = ({location}) => {
+
+    
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search);
+
+        console.log(name, room);
+        
+    });
+
     return (
         <h1>Chat</h1>
     )
