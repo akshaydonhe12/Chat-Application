7c4f05a491f9510d41596e3db944397b4f8226c9
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-06-17 11:46:32 +0530

    create-addUser

--- server/users.js
+++ server/users.js
@@ -1,6 +1,17 @@
 const users= [];
 
-const addUser = () => {
+const addUser = ({id, name, room}) => {
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    const existingUser = users.find((user) => user.room === room && user.name === name);
+     
+    if(existingUser) {
+        return {error:"Username already exits"};
+    }
+        const user = {id, name, room};
+        users.push(user);
+        return {user}
 
 }
 
