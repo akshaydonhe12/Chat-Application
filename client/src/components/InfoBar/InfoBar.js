ba84c436bb7e92e9d47de7751ca5a2267a56e76c
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-06-19 11:54:05 +0530

    changesIn-Ui

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -11,7 +11,7 @@ const InfoBar = ({room}) => (
             <h3>{room}</h3>
         </div>
         <div className="righInnerContainer">
-            <a href="/"><img src={closeIcon} alt="close image" /></a>
+            <a href="/"><img className="closeIcon" src={closeIcon} alt="close image" /></a>
         </div>
     </div>
 )
